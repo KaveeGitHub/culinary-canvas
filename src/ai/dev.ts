c024bf70a15c57b7fca7c35d4e8b6eefb2720af0
BlueWave <117451172+KaveeGitHub@users.noreply.github.com>
2025-07-07 23:31:03 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/detect-food.ts';
-import '@/ai/flows/suggest-recipes.ts';
-import '@/ai/flows/generate-recipe.ts';
-import '@/ai/flows/generate-steps.ts';
-import '@/ai/flows/text-to-speech.ts';
-import '@/ai/flows/generate-image.ts';
