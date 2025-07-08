026afd5f362763789b9f1b04cbba8c003ce6373f
BlueWave <117451172+KaveeGitHub@users.noreply.github.com>
2025-07-08 11:43:02 +0530

    Update dev.ts

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -5,6 +5,7 @@ import '@/ai/flows/detect-food.ts';
 import '@/ai/flows/suggest-recipes.ts';
 import '@/ai/flows/generate-recipe.ts';
 import '@/ai/flows/generate-steps.ts';
-import '@/ai/flows/text-to-speech.ts';
 import '@/ai/flows/generate-image.ts';
 import '@/ai/flows/ask-chef.ts';
+
+    
